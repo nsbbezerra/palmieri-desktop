119074fcaa87d57b06b9f9df775d89ee77ed819b
nsbbezerra <nsbbezerra@hotmail.com>
2020-08-04 15:37:40 -0300

    adjusts

--- public/electron.js
+++ public/electron.js
@@ -24,6 +24,7 @@ function createWindow() {
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
+  mainWindow.webContents.openDevTools();
   mainWindow.on("closed", () => (mainWindow = null));
 }
 
